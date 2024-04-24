863d3e2a1929206a1bed0e4171568f6a315d0d87
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-04-23 23:16:07 -0500

    practice building endpoints

--- src/main/java/com/aya/controller/ApiController.java
+++ src/main/java/com/aya/controller/ApiController.java
@@ -1,14 +1,17 @@
 package com.aya.controller;
 
 
+import com.aya.dto.AddressDTO;
 import com.aya.dto.ResponseWrapper;
 import com.aya.dto.StudentDTO;
 import com.aya.dto.TeacherDTO;
+import com.aya.service.AddressService;
+import com.aya.service.ParentService;
 import com.aya.service.StudentService;
 import com.aya.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +20,15 @@ public class ApiController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
+    private final AddressService addressService;
 
 
-    public ApiController(TeacherService teacherService, StudentService studentService) {
+    public ApiController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
+        this.addressService = addressService;
     }
 
     @GetMapping("/teachers")
@@ -34,8 +41,51 @@ public class ApiController {
     @GetMapping("/students")
     public ResponseEntity<ResponseWrapper> readAllStudents(){
 
-        return ResponseEntity.ok(new ResponseWrapper("test",studentService.findAll()));
+        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll()));
 
     }
+
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+
+        ResponseWrapper response=
+                new ResponseWrapper(true,"Parents are successfully retrieved", HttpStatus.OK.value(), parentService.findAll());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id")Long id) throws Exception {
+
+        AddressDTO addressToReturn=addressService.findById(id);
+
+        return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved",addressToReturn));
+
+    }
+
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id")Long id,
+                                                         @RequestBody AddressDTO addressDTO) throws Exception {
+
+        addressDTO.setId(id);
+
+        AddressDTO addressToReturn=addressService.update(addressDTO);
+
+        return addressToReturn;
+    }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<ResponseWrapper> readAllAddresses(){
+
+        ResponseWrapper response=
+                new ResponseWrapper(true,"Addresses are successfully retrieved", HttpStatus.OK.value(), addressService.findAll());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+
 
 }
